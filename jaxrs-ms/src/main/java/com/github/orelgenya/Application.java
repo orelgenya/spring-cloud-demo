5bc11eb533a0f96afb8126dfb84b1425a93af023
Evgenii_Orel <evgenii_orel@epam.com>
2017-03-06 12:54:24 +0300

    fixed hazelcast jcache support

--- jaxrs-ms/src/main/java/com/github/orelgenya/Application.java
+++ jaxrs-ms/src/main/java/com/github/orelgenya/Application.java
@@ -1,8 +1,14 @@
 package com.github.orelgenya;
 
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,5 +16,17 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    @Profile("!standalone")
+    HazelcastInstance hazelcastInstance() {
+        return HazelcastClient.newHazelcastClient();
+    }
+
+
+    @Bean
+    CacheManager cacheManager() {
+        return new HazelcastCacheManager(hazelcastInstance()); // (3)
     }
 }
